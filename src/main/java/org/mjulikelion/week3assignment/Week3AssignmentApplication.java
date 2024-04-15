1ec0bc2b828e2de9802d4887fa2d6c37ebbe675d
bonseung <bs.koo@sqisoft.com>
2024-04-15 16:16:29 +0900

    first commit

---
+++ src/main/java/org/mjulikelion/week3assignment/Week3AssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.week3assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mjulikelion/week3assignment/Week3AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.week3assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
